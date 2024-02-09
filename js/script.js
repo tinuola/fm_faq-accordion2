0c5934b0ec572ccaa8e19690ff2f30ecc020429c
tinuola <24995224+tinuola@users.noreply.github.com>
2024-02-08 22:32:58 -0800

    feat: add functionality to accordion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
-console.log('Hello!')
+const tabs = document.querySelectorAll('.accordion__trigger')
+
+
+tabs.forEach( tab => {
+  tab.addEventListener('click', toggleAccordion)
+})
+
+
+// # Model
+// Source: https://accessible-components.sparkbox.com/accordion
+
+function toggleAccordion(e){
+
+  let isPanelOpen = e.target.getAttribute('aria-expanded') === 
+  'true' || false;
+  // ! e.target.getAttribute('aria-expanded') returns string, not a boolean
+
+  /* * 
+  * To have single panel open at a time, use
+  * collapseAll method and comment out `else` block
+  */
+
+  // collapseAll()
+
+  if(isPanelOpen){
+    e.target.setAttribute('aria-expanded', false)
+    e.target.parentElement.classList.add('collapsed')
+  } else {
+    e.target.setAttribute('aria-expanded', true)
+    e.target.parentElement.classList.remove('collapsed')
+  } 
+}
+
+
+// function collapseAll(){
+//   tabs.forEach (tab => {
+//     tab.parentElement.classList.add('collapsed')
+//     tab.setAttribute('aria-expanded', false)
+//   })
+// }
+
+
+
+// # Alternate Approach
+// Source: https://www.aditus.io/patterns/accordion/
+
+/* 
+function toggleAccordion(e){
+  let target = e.target.parentElement.nextElementSibling
+  let expanded = e.target.getAttribute('aria-expanded') === 'true' || false;
+  e.target.setAttribute('aria-expanded', !expanded);
+  target.hidden = expanded;
+}
+*/
