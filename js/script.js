f7f1459c4db9c11d943a9f8090660a64e571f31f
tinuola <24995224+tinuola@users.noreply.github.com>
2024-03-22 23:05:06 -0700

    fix: deco image responsive rendering, add to separate elem, not body

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
 const tabs = document.querySelectorAll('.accordion__trigger')
 
-
-tabs.forEach( tab => {
+tabs.forEach((tab) => {
   tab.addEventListener('click', toggleAccordion)
 })
-
 
 // # Resource
 // https://accessible-components.sparkbox.com/accordion
 
-function toggleAccordion(e){
-  let isPanelOpen = e.target.getAttribute('aria-expanded') === 
-  'true' || false;
+function toggleAccordion(e) {
+  let isPanelOpen = e.target.getAttribute('aria-expanded') === 'true' || false
+  console.log(e.target.getAttribute('aria-expanded'))
+  console.log(isPanelOpen)
   // ! e.target.getAttribute('aria-expanded') returns string, not a boolean
 
-  /* * 
-  * To have a single panel open at a time, enable the
-  * collapseAll method and comment out the 'else' block
-  */
+  /* *
+   * To have a single panel open at a time, enable the
+   * collapseAll method and comment out the 'else' block
+   */
   // collapseAll()
-
 
   /* *
    * Adding the 'hidden' attribute to the div/panel markup
    * prevents users and screenreaders from accessing hidden
    * panels inadvertently
-  */
-  if(isPanelOpen){
+   */
+  if (isPanelOpen) {
     e.target.setAttribute('aria-expanded', false) // btn
     e.target.parentElement.classList.add('collapsed') // h3
     e.target.parentElement.nextElementSibling.hidden = isPanelOpen // div
@@ -34,9 +32,8 @@ function toggleAccordion(e){
     e.target.setAttribute('aria-expanded', true)
     e.target.parentElement.classList.remove('collapsed')
     e.target.parentElement.nextElementSibling.hidden = isPanelOpen
-  } 
+  }
 }
-
 
 // function collapseAll(){
 //   tabs.forEach (tab => {
@@ -44,8 +41,6 @@ function toggleAccordion(e){
 //     tab.setAttribute('aria-expanded', false)
 //   })
 // }
-
-
 
 // # Alternate Approach
 // https://www.aditus.io/patterns/accordion/
